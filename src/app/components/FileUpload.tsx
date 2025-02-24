be6c1b108dc977b1886bb35b3ad398646fec3308
Andrew Capaldo <andycapaldo2@gmail.com>
2025-02-24 13:03:40 -0700

    Added file upload comp

---
+++ src/app/components/FileUpload.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+interface FileUploadProps {
+    onFileChange: (file: File) => void;
+}
+
+export default function FileUpload({ onFileChange }: FileUploadProps) {
+    const [file, setFile] = useState<File | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const uploadedFile = e.target.files?.[0];
+        if (uploadedFile) {
+            if (uploadedFile.type === "application/pdf") {
+                setFile(uploadedFile);
+                setError(null);
+                onFileChange(uploadedFile);
+            } else {
+                setFile(null);
+                setError("Please upload a PDF file.");
+            }
+        }
+    }
+
+return (
+        <div>
+            <input type="file" onChange={handleChange}/>
+            {file && <p>{file.name}</p>}
+            {error && <p className="text-red-500">{error}</p>}
+        </div>
+    )
+}
--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,29 +1,50 @@
-import { FaUserAlt, FaEnvelope, FaLock } from 'react-icons/fa';
+'use client'
+
+import FileUpload from "./FileUpload";
+import { useState } from 'react';
 
 export default function Form() {
+
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (uploadedFile: File) => {
+        setFile(uploadedFile);
+    }
+
+    const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        const formData = new FormData(e.target as HTMLFormElement);
+        if (file) {
+            formData.append('file', file);
+        }
+        const payload = Object.fromEntries(formData); 
+
+        console.log(payload);
+    }
+
     return (
         <form className="bg-gray-50 shadow-md 
                         rounded px-8 pt-6 pb-8 
-                        mb-4 max-w-md w-full ">
+                        mb-4 max-w-md w-full" onSubmit={submitForm}>
             <h1 className='font-bold mb-2'>Job Info</h1>
             <div className='border-2 border-gray'>
-                <input className='rounded-t-sm bg-gray-100 w-full' placeholder='job title' />
+                <input name="jobTitle" className='rounded-t-sm bg-gray-100 w-full' placeholder='job title' />
             </div>
             <div className='border-2 border-gray'>
-                <input className='rounded-none bg-gray-100 w-full' placeholder='company' />
+                <input name="company" className='rounded-none bg-gray-100 w-full' placeholder='company' />
             </div>
             <div className='border-2 border-gray'>
-                <input className='rounded-none bg-gray-100 w-full' placeholder='location' />
+                <input name="location" className='rounded-none bg-gray-100 w-full' placeholder='location' />
             </div>
             <div className='border-2 border-gray w-full'>
-                <textarea className='rounded-none bg-gray-100 w-full h-full' placeholder='copy and paste the job description here' />
+                <textarea name="description" className='rounded-none bg-gray-100 w-full h-full' placeholder='copy and paste the job description here' />
             </div>
             <div className="border-2 border-gray rounded-none bg-gray-100 w-full">
-                <button className='rounded bg-black text-white' type="submit">Upload Resume</button>
-                <h1 className='text-sm'>Upload a PDF of Your Resume/CV</h1>
+                <FileUpload onFileChange={handleFileChange} />
             </div>
             <div className='flex justify-center'>
-                <button className='rounded bg-black text-white mt-5 w-1/2'>Submit</button>
+                <button type="submit" className='rounded bg-black text-white mt-5 w-1/2'>Submit</button>
             </div>
         </form>
     )
